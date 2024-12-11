5f29685e4c0d998dbcb02cb766e867c5736a6302
Lucas sobczak <lucassobczak3@gmail.com>
2024-12-10 23:16:59 -0500

    added content sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
+import SectionContainer from "./components/sectionContainer/SectionContainer";
 
 function App() {
+    const sections = [
+        {"title": "About", "content": "content"},
+        {"title": "Projects", "content": "content"},
+        {"title": "Contact", "content": "content"}
+    ]
+
   return (
     <div className="bg-main-bg h-dvh text-white">
       <Header />
+      {sections.map((section, id) => {
+        return (
+            <>
+                <SectionContainer
+                key={id}
+                title={section.title}
+                />
+            </>
+        )
+      })}
+      {/* render each section here?
+      or
+      sep with addition component */}
+      {/* footer */}
     </div>
   );
 }
---
+++ src/components/sectionContainer/SectionContainer.jsx
@@ -0,0 +1,9 @@
+export default function SectionContainer({title, value, child}) {
+  return (
+    <div>
+        <p>test title</p>
+        {/* render child */}
+        <span>01</span>
+    </div>
+  )
+}
