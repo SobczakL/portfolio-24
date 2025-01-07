b540f7bed8749477d3270a4331a0f6cdbf717a92
Lucas sobczak <lucassobczak3@gmail.com>
2025-01-06 22:57:48 -0500

    created binary replacement for each about sectence

--- src/components/content/AboutContent.jsx
+++ src/components/content/AboutContent.jsx
@@ -1,16 +1,42 @@
-import { useEffect, useState } from "react";
-import { useIntersectionObserver } from "../utils/useIntersectionObserver";
+import { useState } from "react";
 
 export default function AboutContent() {
     const content = [
-        { id: "content-0", text: "Self-taught Frontend Developer" },
-        { id: "content-1", text: "E-commerce background with a user-first mindset" },
-        { id: "content-2", text: "Impactful performance responsive design" },
-        { id: "content-3", text: "Analytical creative problem-solver" },
-        { id: "content-4", text: "Tutor and mentor to aspiring developers" },
-        { id: "content-5", text: "Solution driven" },
-        { id: "content-6", text: "Entrepreneurial spirit" },
+        "Self-taught Frontend Developer",
+        "E-commerce background with a user-first mindset",
+        "Impactful performance responsive design",
+        "Analytical creative problem-solver",
+        "Tutor and mentor to aspiring developers",
+        "Solution driven",
+        "Entrepreneurial spirit"
     ];
+
+    const createPlaceholderBinary = (arr) => {
+        let maxChar = 0
+        arr.forEach((sentence) => {
+            const longestWordLength = sentence.split(" ").sort((prev, curr) => curr.length > - prev.length)[0].length
+
+            if(maxChar < longestWordLength){
+                maxChar = longestWordLength
+            }
+        })
+        let curLine = []
+        for (let i = 0; i < maxChar; i++) {
+            let randomIndex = Math.round(Math.random())
+            curLine.push(randomIndex.toString())
+        }
+        console.log(curLine.join(''))
+        return curLine.join('')
+    }
+
+console.log(createPlaceholderBinary(content))
+    const [placeholderText, setPlaceholderText] = useState(() => {
+        let initState = []
+        content.map((sentence) => {
+            initState.push(createPlaceholderBinary(sentence))
+        })
+        return initState
+    })
 
     return (
         <div className="h-full"
